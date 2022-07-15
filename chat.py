ae900ad0d3e55d11b47983e3222fe6eb526191e8
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2022-07-15 22:36:25 +0530

    Department chat bot final files

--- chat.py
+++ chat.py
@@ -1,14 +1,14 @@
 import random
-import json 
+import json
 
 import torch
 
-from model import NeuralNet 
+from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json','r') as json_data:
+with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
 FILE = "data.pth"
@@ -27,7 +27,7 @@ model.eval()
 
 bot_name = "Ayush"
 
-def get_message(msg):
+def get_response(msg):
     sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
@@ -43,12 +43,13 @@ def get_message(msg):
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent["tag"]:
-                return random.choice(intent['resposes'])
-        
-    return "I don't understand..."
+                return random.choice(intent['responses'])
+    
+    return "I do not understand..."
+
 
 if __name__ == "__main__":
-    print("Lest'chat! (type 'quit' to exit)")
+    print("Let's chat! (type 'quit' to exit)")
     while True:
         # sentence = "do you use credit cards?"
         sentence = input("You: ")
@@ -57,3 +58,4 @@ if __name__ == "__main__":
 
         resp = get_response(sentence)
         print(resp)
+
--- nltk_utils.py
+++ nltk_utils.py
@@ -1,23 +1,28 @@
-import numpy as np 
+import numpy as np
 import nltk
-# nltk.download('punkt)
+# nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
-stemmer = PorterStemmer
+stemmer = PorterStemmer()
+
+
 def tokenize(sentence):
-    """.git\split sentence into array of words/tokens
+    """
+    split sentence into array of words/tokens
     a token can be a word or punctuation character, or number
     """
     return nltk.word_tokenize(sentence)
 
+
 def stem(word):
     """
     stemming = find the root form of the word
-    exaples:
-    words = ["orgeanize", "organizes", "organizing"]
+    examples:
+    words = ["organize", "organizes", "organizing"]
     words = [stem(w) for w in words]
     -> ["organ", "organ", "organ"]
     """
     return stemmer.stem(word.lower())
+
 
 def bag_of_words(tokenized_sentence, words):
     """
@@ -26,14 +31,14 @@ def bag_of_words(tokenized_sentence, words):
     example:
     sentence = ["hello", "how", "are", "you"]
     words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-    bag = [0, 1, 0, 1, 0, 0, 0] 
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
     """
-
     # stem each word
     sentence_words = [stem(word) for word in tokenized_sentence]
-    # intialize bag with 0 or each word
+    # initialize bag with 0 for each word
     bag = np.zeros(len(words), dtype=np.float32)
-    for idx, w in wnumerate(words):
-        if w in sentence_words:
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
             bag[idx] = 1
+
     return bag
